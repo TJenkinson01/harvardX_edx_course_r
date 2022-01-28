9262fe117bcf37093ffc6b0fa3817a4a3a568532
Tom Jenkinson <89093128+TJenkinson01@users.noreply.github.com>
2022-01-28 10:38:20 +0100

    file upload

---
+++ multivariate_regression.R
@@ -0,0 +1,155 @@
+library(tidyverse)
+library(dplyr)
+library(dslabs)
+library(stringr)
+library(remotes)
+library(HistData)
+library(Lahman)
+
+
+# - - - - - - - - - - - baseball example:
+# hr: home run
+# bb: balls on base
+
+# stratification and sultivariate regression
+
+# stratify hr per game to nearest 10, filter out strata with few points
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(HR_strata = round(HR/G, 1), 
+         BB_per_game = BB / G,
+         R_per_game = R / G) %>%
+  filter(HR_strata >= 0.4 & HR_strata <=1.2)
+
+# scatterplot for each hr stratum
+dat %>% 
+  ggplot(aes(BB_per_game, R_per_game)) +  
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap( ~ HR_strata)
+
+# calculate slope of regression line after stratifying by hr
+dat %>%  
+  group_by(HR_strata) %>%
+  summarize(slope = cor(BB_per_game, R_per_game)*sd(R_per_game)/sd(BB_per_game))
+
+# stratify by bb
+dat <- Teams %>% filter(yearID %in% 1961:2001) %>%
+  mutate(BB_strata = round(BB/G, 1), 
+         HR_per_game = HR / G,
+         R_per_game = R / G) %>%
+  filter(BB_strata >= 2.8 & BB_strata <=3.9) 
+
+# scatterplot for each bb stratum
+dat %>% ggplot(aes(HR_per_game, R_per_game)) +  
+  geom_point(alpha = 0.5) +
+  geom_smooth(method = "lm") +
+  facet_wrap( ~ BB_strata)
+
+# slope of regression line after stratifying by bb
+dat %>%  
+  group_by(BB_strata) %>%
+  summarize(slope = cor(HR_per_game, R_per_game)*sd(R_per_game)/sd(HR_per_game)) 
+
+#test
+
+fit <- Teams %>% filter(yearID %in% 1961:2001) %>% 
+  mutate(R_per_game = R / G, BB_per_game = BB / G, HR_per_game = HR / G) %>% 
+  lm(R_per_game ~ BB_per_game + HR_per_game, data = .)
+summary(fit)
+
+
+
+
+# - - - - - - - - - - family heights example:
+
+# fit regression line to predict son's height from father's height
+fit <- lm(son ~ father, data = galton_heights)
+fit
+
+summary(fit)
+
+# monte carlo simulation
+B <- 1000
+N <- 50
+lse <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>% 
+    lm(son ~ father, data = .) %>% 
+    .$coef 
+})
+lse <- data.frame(beta_0 = lse[1,], beta_1 = lse[2,]) 
+
+
+# plot the distribution of beta_0 and beta_1
+p1 <- lse %>% ggplot(aes(beta_0)) + geom_histogram(binwidth = 5, color = "black") 
+p2 <- lse %>% ggplot(aes(beta_1)) + geom_histogram(binwidth = 0.1, color = "black") 
+grid.arrange(p1, p2, ncol = 2)
+
+
+# lse can be strongly correlated, which can be seen using this code:
+lse %>% summarize(cor(beta_0, beta_1))
+
+# correlation depends on how the predictors are defined or transformed
+
+# standardise the father heights, which changes "xi" to "xi - x bar".
+B <- 1000
+N <- 50
+lse <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>%
+    mutate(father = father - mean(father)) %>%
+    lm(son ~ father, data = .) %>% .$coef 
+})
+
+# observe what happens to the correlation in this case:
+cor(lse[1,], lse[2,]) 
+
+# compute RSS for any pair of beta0 and beta1 in Galton's data
+galton_heights <- GaltonFamilies %>%
+  filter(gender == "male") %>%
+  group_by(family) %>%
+  sample_n(1) %>%
+  ungroup() %>%
+  select(father, childHeight) %>%
+  rename(son = childHeight)
+rss <- function(beta0, beta1){
+  resid <- galton_heights$son - (beta0+beta1*galton_heights$father)
+  return(sum(resid^2))}
+
+# plot in 2d at a set value for beta 0 (input value as preferred)
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <- data.frame(beta1 = beta1,
+                      rss = sapply(beta1, rss, beta0 = 36))
+results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+  geom_line(aes(beta1, rss), col=2)
+
+
+B <- 1000
+N <- 100
+lse <- replicate(B, {
+  sample_n(galton_heights, N, replace = TRUE) %>% 
+    lm(son ~ father, data = .) %>% .$coef 
+})
+
+lse <- data.frame(beta_0 = lse[1,], beta_1 = lse[2,]) 
+
+set.seed(1989, sample.kind="Rounding") 
+
+data("GaltonFamilies")
+options(digits = 3)    # report 3 significant digits
+
+female_heights <- GaltonFamilies %>%     
+  filter(gender == "female") %>%     
+  group_by(family) %>%     
+  sample_n(1) %>%     
+  ungroup() %>%     
+  select(mother, childHeight) %>%     
+  rename(daughter = childHeight)
+
+fit <- lm(mother ~ daughter, data = female_heights)
+fit
+
+summary(fit)
+
+predict(fit)[1]
+
+female_heights$mother[1]
+

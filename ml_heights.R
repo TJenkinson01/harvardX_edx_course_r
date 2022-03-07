cdef6ac2134843f5f068afe1710980eeabdffd7f
Tom Jenkinson <89093128+TJenkinson01@users.noreply.github.com>
2022-03-07 11:57:15 +0100

    initial upload

---
+++ ml_heights.R
@@ -0,0 +1,70 @@
+library(tidyverse)
+library(caret)
+library(dslabs)
+data(heights)
+
+# define the outcome and predictors
+
+y <- heights$sex
+x <- heights$height
+
+# generate training and test sets
+
+set.seed(2, sample.kind = "Rounding") # if using R 3.5 or earlier, remove the sample.kind argument
+test_index <- createDataPartition(y, times = 1, p = 0.5, list = FALSE)
+test_set <- heights[test_index, ]
+train_set <- heights[-test_index, ]
+
+# guess the outcome
+
+y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE)
+y_hat <- sample(c("Male", "Female"), length(test_index), replace = TRUE) %>% 
+  factor(levels = levels(test_set$sex))
+
+# compute Overall Accuracy
+
+mean(y_hat == test_set$sex)
+heights %>% group_by(sex) %>% summarize(mean(height), sd(height))
+y_hat <- ifelse(x > 62, "Male", "Female") %>% factor(levels = levels(test_set$sex))
+mean(y == y_hat)
+
+# examine the accuracy of 10 cutoffs
+
+cutoff <- seq(61, 70)
+accuracy <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  mean(y_hat == train_set$sex)
+})
+
+# plot accuracy graphically
+
+data.frame(cutoff, accuracy) %>% 
+  ggplot(aes(cutoff, accuracy)) + 
+  geom_point() + 
+  geom_line() 
+max(accuracy)
+
+
+# recreate algorithm to maximize F-score instead of Overall Accuracy
+
+cutoff <- seq(61, 70)
+F_1 <- map_dbl(cutoff, function(x){
+  y_hat <- ifelse(train_set$height > x, "Male", "Female") %>% 
+    factor(levels = levels(test_set$sex))
+  F_meas(data = y_hat, reference = factor(train_set$sex))
+})
+
+
+data.frame(cutoff, F_1) %>% 
+  ggplot(aes(cutoff, F_1)) + 
+  geom_point() + 
+  geom_line()
+
+max(F_1)
+
+# define which height is best cut off with respect to F-score
+
+best_cutoff <- cutoff[which.max(F_1)]
+best_cutoff
+
